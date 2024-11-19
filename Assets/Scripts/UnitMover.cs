f0ce4fa6dea2680c855d1c4b4d41a262b4281162
f56e751 <fortriver54321@gmail.com>
2024-11-19 09:32:08 +0900

    학생 도트 추가

--- Assets/Scripts/UnitMover.cs
+++ Assets/Scripts/UnitMover.cs
@@ -19,9 +19,7 @@ public class UnitMover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // TestInstanciate(unitToMove);
 
-        
     }
 
     // Update is called once per frame
@@ -121,79 +119,4 @@ public class UnitMover : MonoBehaviour
             yield return null; // 다음 프레임까지 대기
         }
     }
-
-
-    // public void MoveUnit((int x, int z) targetPosition)
-    // {       
-    //     float unitXposition = unitToMove.transform.position.x;
-    //     float unitZposition = unitToMove.transform.position.z;
-    //     float leftDistance = (float) Math.Sqrt(Math.Pow(unitXposition - targetPosition.x, 2) + Math.Pow(unitZposition - targetPosition.z, 2));
-        
-    //     Debug.Log("unitXposition: " + unitXposition);
-    //     Debug.Log("unitZposition: " + unitZposition);
-
-    //     Debug.Log("targetPosition.x: " + targetPosition.x);
-    //     Debug.Log("targetPosition.z: " + targetPosition.z);
-
-
-    //     // 타겟 위치까지 왔는지 확인
-    //     if (leftDistance < moveThreshold) 
-    //     {
-    //         isArrived = true;
-    //         isFirstSearch = true;
-    //         shortestPath.Clear();
-    //         // Debug.Log("leftDistance < moveThreshold");
-    //         return;
-    //     }
-    //     else 
-    //     {
-    //         isArrived = false;
-    //     }
-
-    //     // BuildManager와 graph가 올바르게 초기화되었는지 확인
-    //     if (BuildManager.Instance == null || BuildManager.Instance.graph == null)
-    //     {
-    //         Debug.LogError("BuildManager or graph is not initialized!");
-    //         return;
-    //     }
-
-    //     // 연산량을 줄이기 위해 움직이기 시작했을때만 최단경로 탐색을 진행함
-    //     if (isFirstSearch)
-    //     {
-    //         isFirstSearch = false;
-    //         shortestPath = BuildManager.Instance.graph.GetShortestPath(((int)unitXposition, (int)unitZposition), targetPosition);
-
-    //         if (shortestPath == null || shortestPath.Count == 0)
-    //         {
-    //             Debug.LogError("Shortest path could not be found or is empty!");
-    //             return;
-    //         }
-    //     }
-
-
-    //     // 이동 도중에 path에 건물이 생긴 경우 경로를 다시 탐색함
-    //     if (shortestPath != null && isPathOccupied())
-    //     {   
-    //         shortestPath = BuildManager.Instance.graph.GetShortestPath(((int) unitXposition, (int) unitZposition), targetPosition);
-    //     }
-
-
-    //     if (shortestPath != null && shortestPath.Count > 0)
-    //     {
-    //         Vector3 nextPosition = shortestPath[0]; // Get the next position from the path
-    //         nextPosition.y = yAxis;
-    //         Vector3 moveDirection = (nextPosition - unitToMove.transform.position).normalized; // Calculate the direction to move in
-    //         // moveDirection.y = 0;
-    //         unitToMove.transform.position += moveDirection * speed * Time.deltaTime; // Move the unit
-
-    //         // Check if close enough to next waypoint to consider it "reached"
-    //         if (Vector3.Distance(unitToMove.transform.position, nextPosition) < moveThreshold)
-    //         {
-    //             shortestPath.RemoveAt(0); // Remove the reached waypoint from the path
-    //         }
-    //     }
-
-
-    // }
-
 }
